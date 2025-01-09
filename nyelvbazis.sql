cd209f5476212c69b52df147123258646d685790
KapiR1 <90643709+KapiR1@users.noreply.github.com>
2025-01-09 08:08:51 +0100

    mondatok hozzáadása

--- nyelvbazis.sql
+++ nyelvbazis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Dec 22. 15:37
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.0.30
+-- Létrehozás ideje: 2025. Jan 09. 08:05
+-- Kiszolgáló verziója: 10.4.20-MariaDB
+-- PHP verzió: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `mondatok_magyar`
+--
+
+CREATE TABLE `mondatok_magyar` (
+  `Id` int(11) NOT NULL,
+  `magyar_mondatok` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `mondatok_magyar`
+--
+
+INSERT INTO `mondatok_magyar` (`Id`, `magyar_mondatok`) VALUES
+(1, 'Szeretek könyveket olvasni a szabadidőmben.'),
+(2, 'Tudsz segíteni nekem ebben a matematika háziban?'),
+(3, 'A nap ma nagyon fényesen süt.'),
+(4, 'A barátaimmal ma este moziba megyünk.'),
+(5, 'A lány gitározni tanul.'),
+(6, 'Tegnap egy ízletes pizzát ettem.'),
+(7, 'Merre van a mosdó?'),
+(8, 'Ez a könyv nagyon érdekes és oktató jellegű.'),
+(9, 'Nyaralást tervezünk a tengerparton.'),
+(10, 'Szeretek zenét hallgatni tanulás közben.');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `mondatok_spanyol`
+--
+
+CREATE TABLE `mondatok_spanyol` (
+  `Id` int(11) NOT NULL,
+  `spanyol_mondatok` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `mondatok_spanyol`
+--
+
+INSERT INTO `mondatok_spanyol` (`Id`, `spanyol_mondatok`) VALUES
+(1, 'Me gusta leer libros en mi tiempo libre.'),
+(2, '¿Puedes ayudarme con esta tarea de matemáticas?'),
+(3, 'El sol brilla muy fuerte hoy.'),
+(4, 'Mis amigos y yo vamos al cine esta noche.'),
+(5, 'La niña está aprendiendo a tocar la guitarra.'),
+(6, 'Ayer comí una pizza deliciosa.'),
+(7, '¿Dónde está el baño?'),
+(8, 'Este libro es muy interesante y educativo.'),
+(9, 'Estamos planeando unas vacaciones en la playa.'),
+(10, 'Me encanta escuchar música mientras estudio.');
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `profil`
 --
 
@@ -33,7 +87,7 @@ CREATE TABLE `profil` (
   `email` varchar(50) NOT NULL,
   `jelszo` varchar(32) NOT NULL,
   `pontszam` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -44,7 +98,7 @@ CREATE TABLE `profil` (
 CREATE TABLE `szavak_magyar` (
   `id` int(11) NOT NULL,
   `magyar_szo` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- A tábla adatainak kiíratása `szavak_magyar`
@@ -111,7 +165,7 @@ INSERT INTO `szavak_magyar` (`id`, `magyar_szo`) VALUES
 CREATE TABLE `szavak_spanyol` (
   `id` int(11) NOT NULL,
   `spanyol_szo` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- A tábla adatainak kiíratása `szavak_spanyol`
@@ -174,6 +228,18 @@ INSERT INTO `szavak_spanyol` (`id`, `spanyol_szo`) VALUES
 --
 
 --
+-- A tábla indexei `mondatok_magyar`
+--
+ALTER TABLE `mondatok_magyar`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A tábla indexei `mondatok_spanyol`
+--
+ALTER TABLE `mondatok_spanyol`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- A tábla indexei `profil`
 --
 ALTER TABLE `profil`
@@ -196,6 +262,18 @@ ALTER TABLE `szavak_spanyol`
 --
 
 --
+-- AUTO_INCREMENT a táblához `mondatok_magyar`
+--
+ALTER TABLE `mondatok_magyar`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT a táblához `mondatok_spanyol`
+--
+ALTER TABLE `mondatok_spanyol`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT a táblához `profil`
 --
 ALTER TABLE `profil`
@@ -212,6 +290,22 @@ ALTER TABLE `szavak_magyar`
 --
 ALTER TABLE `szavak_spanyol`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `mondatok_spanyol`
+--
+ALTER TABLE `mondatok_spanyol`
+  ADD CONSTRAINT `mondatok_spanyol_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `mondatok_magyar` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Megkötések a táblához `szavak_magyar`
+--
+ALTER TABLE `szavak_magyar`
+  ADD CONSTRAINT `szavak_magyar_ibfk_1` FOREIGN KEY (`id`) REFERENCES `szavak_spanyol` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
