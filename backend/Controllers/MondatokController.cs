6d39d5c04c608f979260663084cafa327a95d4e2
Fegyverneki <90623178+Fegyverneki@users.noreply.github.com>
2025-02-04 14:15:36 +0100

    Mondatok végpontja megvalósítva

---
+++ backend/Controllers/MondatokController.cs
@@ -0,0 +1,58 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MondatokController : ControllerBase
+    {
+        [HttpGet("GetAllHungarian")]
+        public IActionResult GetAllHungarian()
+        {
+            using (var context = new NyelvbazisContext())
+            {
+                try
+                {
+                    List<MondatokMagyar> result = context.MondatokMagyars.ToList();
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    List<MondatokMagyar> result = new List<MondatokMagyar>();
+                    MondatokMagyar hiba = new MondatokMagyar()
+                    {
+                        Id = -1,
+                        MagyarMondatok = ex.Message
+                    };
+                    result.Add(hiba);
+                    return BadRequest(result);
+                }
+            }
+        }
+        [HttpGet("GetAllSpanish")]
+        public IActionResult GetAllSpanish()
+        {
+            using (var context = new NyelvbazisContext())
+            {
+                try
+                {
+                    List<MondatokSpanyol> result = context.MondatokSpanyols.ToList();
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    List<MondatokSpanyol> result = new List<MondatokSpanyol>();
+                    MondatokSpanyol hiba = new MondatokSpanyol()
+                    {
+                        Id = -1,
+                        SpanyolMondatok = ex.Message
+                    };
+                    result.Add(hiba);
+                    return BadRequest(result);
+                }
+            }
+        }
+    }
+}
