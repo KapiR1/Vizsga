c6b9edf1d9b610893ba6329de3f3b93a598b471c
MacsekLevente <90623199+MacsekLevente@users.noreply.github.com>
2025-02-06 09:56:02 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,37 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // A navbarhoz tartozó stílusokat külön CSS fájlba érdemes tenni
-
 export default function Navbar() {
-  const [scrolling, setScrolling] = useState(false); // Állapot a görgetés figyelésére
-
-  // Ezt az effektust használjuk, hogy figyeljük a görgetést
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {  // Ha 100px-nél többet görgettünk lefelé
-        setScrolling(true);  // A navbar eltűnik
-      } else {
-        setScrolling(false); // Ha a tetejére érünk, visszajön
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll); // A scroll esemény figyelése
-
-    // Cleanup funkció, hogy eltávolítsuk az eseményfigyelőt
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  
 
   return (
-    <nav className={`navbar navbar-expand-lg navbar-light ${scrolling ? 'navbar-hidden' : 'navbar-visible'}`}>
-      <a className="navbar-brand" href="#">Nyelvbázis</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Nyelvbázis</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li className="nav-item active">
             <Link className="nav-link" to="/profil">Profil</Link>
           </li>
           <li className="nav-item">
@@ -40,8 +22,9 @@ export default function Navbar() {
           <li className="nav-item">
             <Link className="nav-link" to="/vizsga">Vizsga</Link>
           </li>
-        </ul>
-      </div>
-    </nav>
+    </ul> 
+  </div>
+</nav>
   );
 }
+
