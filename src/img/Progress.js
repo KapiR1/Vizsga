afbea79fb6242a0ebb0d803801d2bbed79a7edba
MacsekLevente <90623199+MacsekLevente@users.noreply.github.com>
2025-02-14 11:42:12 +0100

    progress bar elkezdése

---
+++ src/img/Progress.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Progress({ value }) {
+    return (
+        <div className="w-full bg-gray-200 rounded h-4">
+            <div
+                className="bg-blue-600 h-4 rounded"
+                style={{ width: `${value}%` }}
+            ></div>
+        </div>
+    );
+}
