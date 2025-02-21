70e861aac2de41601d5ce95dfaf9b42a5d60822f
KapiR <kapir@kkszki.hu>
2025-02-21 11:47:06 +0100

    bővítés a főoldalra

--- src/Fooldal/Fooldal.js
+++ src/Fooldal/Fooldal.js
@@ -15,9 +15,38 @@ export default function Fooldal() {
             
          <Link to="/register"> <button className="linkes"> Regisztráció </button></Link>
          </div>
-                 
-        <h4 className="negyedik">Készítette: Fegyverneki Tamás, Macsek Levente és Kapi Richárd</h4>
-      </div>
+         <br/>
+         <div className="container">
+    <h3 id="felirat">Miért válaszd a mi platformunkat?</h3>
+    <br/>
+    <ul>
+        <li>
+            <strong>Ingyenes hozzáférés:</strong> Nincs szükség előfizetésre, vagy bármilyen rejtett költségre. Minden, amit a nyelvtanuláshoz szükséges, ingyenesen elérhető!
+        </li>
+        <br/>
+        <li>
+            <strong>Rugalmasság:</strong> Akár otthon, akár úton, vagy a munkahelyeden – a spanyol nyelv elsajátításához bárhol és bármikor hozzáférhetsz. A platformunk mobilbarát, így mindig a kezed ügyében lesz!
+        </li>
+        <br/>
+        <li>
+            <strong>Vizsga lehetőség:</strong> Az elméleti tudásod mellé gyakorlati tudást is szerezhetsz. Oldalunk lehetőséget biztosít arra, hogy vizsgát tegyél és hiteles bizonyítványt szerezz a spanyol nyelv tudásodról.
+        </li>
+        <br/>
+        <li>
+            <strong>Szintre szabott tanulás:</strong> Akár most kezded, akár már haladó szinten vagy, a platformunkon lévő tananyagok minden szintnek megfelelően kerültek kialakításra. A kezdőtől a haladó szintig, mindent megtalálsz, amit szükséges tudni.
+        </li>
+        <br/>
+        <li>
+            <strong>Interaktív tanulás:</strong> Módszereink között szerepelnek interaktív gyakorlatok, videók és kvízek, melyek segítenek gyorsabban elsajátítani a nyelvet, miközben élvezetesebbé teszik a tanulást.
+        </li>
+    </ul>
+
+    <p>Ne hagyd ki ezt a lehetőséget! Kezd el a spanyol nyelv tanulását most, és érj el valódi eredményeket – mindezt teljesen ingyenesen!</p>
+    <h4 className="negyedik">Készítette: Fegyverneki Tamás, Macsek Levente és Kapi Richárd</h4>
+</div>
+
+</div>
+      
     );
   }
   
