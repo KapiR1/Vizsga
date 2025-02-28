01ce0107a44504428f4cea7971e4d6df9716e505
MacsekLevente <90623199+MacsekLevente@users.noreply.github.com>
2025-02-28 12:03:02 +0100

    jogosultságok ellenőrzése

--- backend/Controllers/RegistryController.cs
+++ backend/Controllers/RegistryController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using backend.Models;
 
@@ -65,6 +65,7 @@ namespace backend.Controllers
 
                     }
                     user.Aktiv = 1;
+                    user.Jogosultsag = 1;
                     context.Profils.Update(user);
                     await context.SaveChangesAsync();
                     return Ok("Sikeres aktiválás");
