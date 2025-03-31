4eb486e3bcd8014e956642d82c1ac48433c6b0b3
MacsekLevente <90623199+MacsekLevente@users.noreply.github.com>
2025-03-31 12:52:48 +0200

    design (nemmukodik a commit)

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./Navbar.css";
-import LogoTongue from "./img/LogoTongue.png";
+import { Link, useLocation } from "react-router-dom"
+import "./Navbar.css"
+import LogoTongue from "./img/LogoTongue.png"
 
 export default function Navbar() {
+  const location = useLocation()
+  const currentPath = location.pathname
+
   return (
     <header className="navbar-header">
       <nav className="navbar-nav">
         <div className="navbar-container">
           <ul className="navbar-list">
             <li>
-              <Link className="nav-link" to="/profil">Profil</Link>
+              <Link
+                className={`nav-link ${currentPath === "/profil" || currentPath === "/profilstatisztika" ? "active" : ""}`}
+                to="/profil"
+              >
+                Profil
+              </Link>
             </li>
             <li>
-              <Link className="nav-link" to="/szavakmondatok">Szavak és Mondatok</Link>
+              <Link className={`nav-link ${currentPath === "/szavakmondatok" ? "active" : ""}`} to="/szavakmondatok">
+                Szavak és Mondatok
+              </Link>
             </li>
             <li>
-              <Link className="nav-link" to="/vizsga">Vizsga</Link>
+              <Link className={`nav-link ${currentPath === "/vizsga" ? "active" : ""}`} to="/vizsga">
+                Vizsga
+              </Link>
             </li>
           </ul>
           <div className="navbar-right">
-            <img src={LogoTongue} alt="Logo" className="navbar-image" />
+            <img src={LogoTongue || "/placeholder.svg"} alt="Logo" className="navbar-image" />
             <span className="navbar-text">Nyelvbázis</span>
           </div>
         </div>
       </nav>
     </header>
-  );
+  )
 }
+
